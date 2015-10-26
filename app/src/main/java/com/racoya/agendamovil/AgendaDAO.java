c82e66c4d82e5ccdd6bcb2a9e5e9f9326e07bd59
Roland Oviol <rolandoviol@gmail.com>
2015-10-26 06:51:46 -0400

    inicial

---
+++ app/src/main/java/com/racoya/agendamovil/AgendaDAO.java
@@ -0,0 +1,48 @@
+package com.racoya.agendamovil;
+
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by roland on 10/24/15.
+ */
+public class AgendaDAO {
+    public String getJSON(String address){
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(address);
+        try{
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if(statusCode == 200){
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while((line = reader.readLine()) != null){
+                    builder.append(line);
+                }
+            } else {
+                Log.e(MainActivity.class.toString(), "Failedet JSON object");
+            }
+        }catch(ClientProtocolException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+}
---
+++ app/src/main/java/com/racoya/agendamovil/MainActivity.java
@@ -0,0 +1,72 @@
+package com.racoya.agendamovil;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+public class MainActivity extends ActionBarActivity {
+    // Remove the below line after defining your own ad unit ID.
+    private static final String TOAST_TEXT = "Test ads are being shown. "
+            + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
+    private TextView txttitulo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Load an ad into the AdMob banner view.
+        txttitulo = (TextView) findViewById(R.id.txttitulo);
+        AdView adView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .setRequestAgent("android_studio:ad_template").build();
+        adView.loadAd(adRequest);
+
+        // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
+        Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            AgendaDAO agendaDao = new AgendaDAO();
+            String agendaStr = agendaDao.getJSON("http://agenda.racoya.com/horario/1/");
+            try{
+                JSONObject jsonObject = new JSONObject(agendaStr);
+                Log.i(MainActivity.class.getName(), jsonObject.getString("titulo"));
+                txttitulo.setText(jsonObject.getString("titulo"));
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+            finally{
+                System.out.println("Success");
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
